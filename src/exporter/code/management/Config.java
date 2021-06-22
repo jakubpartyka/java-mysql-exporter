7bb8d7392959aff813b124c19aabbc4fcc969188
jakubpartyka <kontakt@jakub-partyka.pl>
2021-06-22 17:31:28 +0200

    config updated

--- src/exporter/code/management/Config.java
+++ src/exporter/code/management/Config.java
@@ -8,12 +8,11 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Config {
-    private static final String CONFIG_PATH = System.getProperty("user.dir") + "/src/config";
-
-    static String DB_ADDRESS, DB_USERNAME, DB_PASSWORD, DB_NAME;
-    static int DB_PORT = 3306, EXPOSE_PORT;
+    private static final String CONFIG_PATH = "config";
+    public static String QUERIES_FILE_PATH = "queries";
+    public static int EXPOSE_PORT = 20026;
     public static LogLevel LOG_LEVEL = LogLevel.INFO;
-
+    public static String EXPORTER_NAMESPACE = "java-mysql-exporter";
 
     public static void readConfig() throws IOException {
         File config = new File(CONFIG_PATH);
@@ -22,13 +21,11 @@ public class Config {
         Properties prop = new Properties();
         prop.load(new FileReader(config));
 
-        // READ PROPERTIES
-        DB_ADDRESS = prop.getProperty("DB_ADDRESS");
-        DB_USERNAME = prop.getProperty("DB_USERNAME");
-        DB_PASSWORD = prop.getProperty("DB_PASSWORD");
-        DB_NAME = prop.getProperty("DB_NAME");
-        DB_PORT = Integer.parseInt(prop.getProperty("DB_PORT", String.valueOf(DB_PORT)));
+        LOG_LEVEL = LogLevel.valueOf(prop.getProperty("LOG_LEVEL",LOG_LEVEL.name()));
         EXPOSE_PORT = Integer.parseInt(prop.getProperty("EXPOSE_PORT", String.valueOf(EXPOSE_PORT)));
+        EXPORTER_NAMESPACE = prop.getProperty("EXPORTER_NAMESPACE", EXPORTER_NAMESPACE);
+        QUERIES_FILE_PATH = prop.getProperty("QUERIES_FILE_PATH", QUERIES_FILE_PATH);
+
 
         Logger.log("config read from " + config.getAbsolutePath(), LogLevel.INFO);
     }
