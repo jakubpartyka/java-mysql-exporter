b911eaf212cffccdf9b5deb5d90147d4fe513d43
jakubpartyka <kontakt@jakub-partyka.pl>
2021-06-22 21:01:54 +0200

    main loop implemented

--- src/exporter/code/management/Main.java
+++ src/exporter/code/management/Main.java
@@ -1,7 +1,30 @@
 package exporter.code.management;
+import exporter.code.logging.LogLevel;
+import exporter.code.logging.Logger;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+@SuppressWarnings({"BusyWait", "InfiniteLoopStatement"})
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Config.readConfig();
+        Config.readQueries();
+        Exporter.start();
+
+        while (true){
+            for (Query query : Query.queries) {
+                try {
+                    double val = DatabaseConnector.executeQuery(query);
+                    Exporter.setMetricValue(query,val);
+                } catch (SQLException e) {
+                    Logger.log("failed to execute query " + query.statement + " due to " + e.getMessage(), LogLevel.ERROR);
+                    e.printStackTrace();
+                }
+            }
+
+            Thread.sleep(Config.INTERVAL);
+        }
 
     }
 }
