cdb5b3b1c177653e2f27553ec9af5decdfe69f43
jakubpartyka <kontakt@jakub-partyka.pl>
2021-06-22 19:51:53 +0200

    createMetrics implemented

--- src/exporter/code/management/Exporter.java
+++ src/exporter/code/management/Exporter.java
@@ -2,15 +2,53 @@ package exporter.code.management;
 
 import exporter.code.logging.LogLevel;
 import exporter.code.logging.Logger;
+import io.prometheus.client.Collector;
 import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.SimpleCollector;
 import io.prometheus.client.exporter.HTTPServer;
 import java.io.IOException;
+import java.util.HashMap;
 
+
+@SuppressWarnings("rawtypes")
 public class Exporter {
-    public static Counter counter_errors = Counter.build().namespace("otomoto_scanner").name("errors").help("Total number of all messages marked with ERROR").register();
+    public static HashMap<Query,SimpleCollector> collectors = new HashMap<>();
+
+    public static Counter counter_errors = Counter.build().namespace(Config.EXPORTER_NAMESPACE).name("counter_errors").help("Total number of errors").register();
+
+    public static void createMetrics(){
+        for (Query q : Query.queries) {
+            if (q.metricType == MetricType.GAUGE){
+                Gauge collector = Gauge.build().namespace(q.namespace).name(q.name).help(q.description).register();
+                collectors.put(q,collector);
+            }
+            else if(q.metricType == MetricType.COUNTER){
+                Counter collector = Counter.build().namespace(q.namespace).name(q.name).help(q.description).register();
+                collectors.put(q,collector);
+            }
+            else
+                Logger.log("undefined metric type (skipping): " + q.metricType,LogLevel.WARN);
+        }
+    }
+
+    public static void setMetricValue(Query query,int value){
+        Collector collector = collectors.get(query);
+        switch (query.metricType){
+            case GAUGE ->
+                    ((Gauge)collector).set(value);
+            case COUNTER -> {
+                double old_val = ((Counter) collector).get();
+                ((Counter) collector).inc(value - old_val);
+            }
+            default ->
+                    Logger.log("undefined metric type: " + query.metricType,LogLevel.ERROR);
+        }
+    }
 
     public static void start(){
         try {
+            createMetrics();
             new HTTPServer(Config.EXPOSE_PORT);
             Logger.log("Metric exporter server stared on port " + Config.EXPOSE_PORT, LogLevel.INFO);
         } catch (IOException e) {
