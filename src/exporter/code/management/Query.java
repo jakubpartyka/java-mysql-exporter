4ed5eb3fe5a6bb4744ab270f11c40335615b4dfb
jakubpartyka <kontakt@jakub-partyka.pl>
2021-06-22 21:01:44 +0200

    field renamed

--- src/exporter/code/management/Query.java
+++ src/exporter/code/management/Query.java
@@ -7,7 +7,7 @@ public class Query {
     public static ArrayList<Query> queries = new ArrayList<>();
 
     String db_address, db_name, db_user, db_pass;
-    String query;
+    String statement;
     String name,namespace,description;
     MetricType metricType;
     int db_port;
@@ -18,7 +18,7 @@ public class Query {
         this.db_user = db_user;
         this.db_pass = db_pass;
         this.name = name;
-        this.query = query;
+        this.statement = query;
         this.namespace = namespace;
         this.description = description;
         this.metricType = metricType;
@@ -30,12 +30,12 @@ public class Query {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Query query1 = (Query) o;
-        return db_port == query1.db_port && Objects.equals(db_address, query1.db_address) && Objects.equals(db_name, query1.db_name) && Objects.equals(db_user, query1.db_user) && Objects.equals(db_pass, query1.db_pass) && Objects.equals(query, query1.query) && Objects.equals(name, query1.name) && Objects.equals(namespace, query1.namespace) && Objects.equals(description, query1.description) && metricType == query1.metricType;
+        return db_port == query1.db_port && Objects.equals(db_address, query1.db_address) && Objects.equals(db_name, query1.db_name) && Objects.equals(db_user, query1.db_user) && Objects.equals(db_pass, query1.db_pass) && Objects.equals(statement, query1.statement) && Objects.equals(name, query1.name) && Objects.equals(namespace, query1.namespace) && Objects.equals(description, query1.description) && metricType == query1.metricType;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(db_address, db_name, db_user, db_pass, query, name, namespace, description, metricType, db_port);
+        return Objects.hash(db_address, db_name, db_user, db_pass, statement, name, namespace, description, metricType, db_port);
     }
 
     @Override
@@ -45,7 +45,7 @@ public class Query {
                 ", db_name='" + db_name + '\'' +
                 ", db_user='" + db_user + '\'' +
                 ", db_pass='" + db_pass + '\'' +
-                ", query='" + query + '\'' +
+                ", query='" + statement + '\'' +
                 ", name='" + name + '\'' +
                 ", namespace='" + namespace + '\'' +
                 ", description='" + description + '\'' +
